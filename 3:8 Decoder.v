82afee6c2b2038c6c200bbab15aa4df7a42ebfa3
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-03-08 20:44:22 +0530

    Update 3:8 Decoder.v

--- 3:8 Decoder.v
+++ 3:8 Decoder.v
@@ -29,4 +29,24 @@ end
 endmodule
 
 
+
+
+
 // 
+module decoder_tb;
+wire [7:0] out;
+reg en;
+reg [2:0] in;
+integer i;
+ 
+decoder3_to_8 dut(in,out,en);
+ 
+initial begin  
+ $monitor( "en=%b, in=%d, out=%b ", en, in, out);
+   for ( i=0; i<16; i=i+1) 
+        begin
+           {en,in}  = i;
+            #1;
+        end
+end
+endmodule
